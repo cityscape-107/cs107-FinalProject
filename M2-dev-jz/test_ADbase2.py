14eba313045cfb8fc191f1ec6d1d69e7e41ddf23
Eleonora <eleonora.shantsila@gmail.com>
2020-11-19 16:50:28 +0000

    adding testing for type errors

--- M2-dev-jz/test_ADbase2.py
+++ M2-dev-jz/test_ADbase2.py
@@ -10,14 +10,34 @@ import pytest
 ############ Testing for AD #####################
 
 #9-check for non float and no int inputs for vals
+def test_val_types():
+    with pytest.raises(TypeError):
+        x = AD('2',1)
+    with pytest.raises(TypeError):
+        x = AD(2+2j,1)
 #12-check for non float and no int inputs for derivs
+def test_der_types():
+    with pytest.raises(TypeError):
+        x = AD(2,'1')
+    with pytest.raises(TypeError):
+        x = AD(2,1+1j)
 #18 - test the printed string is as expected
+def test_repr():
+    x = AD(42,43)
+    assert repr(x) == 'Numerical Value is:\n{}, \nJacobian is:\n{}'.format([42], [43])
+
 #29 - check for attribute error if adding a non-ad var
-#39 - test for negation as with addition
-#49 - subtraction
-#52 - check A-B!=B-A but - of the answer
-#52 - check 0-A=neg(A)
+
+def test_add_non_AD():
+    with pytest.raises(AttributeError):
+        AD(0.3,1) + 7
+
+
 #60 - attribute error when multiplying by non-ad
+
+def test_mult_non_AD():
+    with pytest.raises(AttributeError):
+        AD(0.3,1) * 7
 #65 - check A*B=B*A
 #68 - division testing
 #74 - 0 div attribute error
@@ -84,6 +104,7 @@ def test_log():
 
 ######## Testing Basic functions #######################
 
+# testing addition
 def test_add_const():
     x = AD(3,1)
     z = 3*x + 2
@@ -105,6 +126,47 @@ def test_add_vars():
     assert 3 + 3==z.val
     assert 1 + 1==z.der
 
+#testing negation
+def test_neg():
+    x = (0.5,1)
+    y = -x
+    assert y.val == -x.val
+    assert y.der == -x.der
+    x = (0.5)
+    y = -x
+    assert y.val == -x.val
+    assert y.der == -x.der
+
+#testing subtraction
+def test_sub_const():
+    x = AD(3,1)
+    z = 4*x - 2
+    assert 10 == z.val
+    assert 4 == z.der
+
+def test_sub_const_rev(): #check A-B = -(B-A)
+    x = AD(3,1)
+    z = 3*x - 2
+    z_r = 2 - 3*x
+    assert z.val == - z_r.val
+    assert z.der == - z_r.der
+
+def test_sub_vars():
+    x = AD(3,1)
+    z = 5*x - x
+    assert 4*x.val == z.val
+    assert 4*x.der == z.der
+    assert 15-3==z.val
+    assert 5-1==z.der
+
+def test_sub_0(): #check 0-A=neg(A)
+    x = AD(3,1)
+    z = 0 - x
+    x_neg = -x
+    assert z.val == x_neg.val
+    assert z.der == x_neg.der
+
+#testing multiplication
 def test_mul_const():
     x = AD(3,1)
     z = 3*x + 2
